99b43cacb730c5a57d05cafbcf4ed04d96f2b0c4
Arne Handt <it@handtwerk.de>
2011-10-30 18:40:48 +0100

    initializing async task class via reflection in application onCreate as a workaround to a bug in async task

--- src/com/orm/SugarApp.java
+++ src/com/orm/SugarApp.java
@@ -1,12 +1,18 @@
 package com.orm;
 
-
 public class SugarApp extends android.app.Application {
 
     Database database;
 
     @Override
     public void onCreate() {
+
+        // to make sure async task is initialized on ui thread
+        try {
+            Class.forName("android.os.AsyncTask");
+        } catch (ClassNotFoundException e) {
+            // ignore
+        }
 
         super.onCreate();
         this.database = new Database(this);
